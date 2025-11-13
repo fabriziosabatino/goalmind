0b9ea2e34c56409239455ada95e00b0892f8ff41
Fabrizio Sabatino <fabrizio.sabatino@studenti.unimi.it>
2025-11-13 14:19:10 +0100

    Add all the basic attributes

--- data_structure/player.py
+++ data_structure/player.py
@@ -1,4 +1,21 @@
 class Player:
-    def __init__(self, name, team):
+    def __init__(self, name, team, position,
+                 mins_played, tacklepergame, foulspergame,
+                 xgperninety, shotspergame, xgpershot, keypasspergame,
+                 dribblewonpergame, rating, interceptionpergame,
+                 goal, assisttotal):
         self.name = name
         self.team = team
+        self.position = position
+        self.mins_played = mins_played
+        self.tacklepergame = tacklepergame
+        self.foulspergame = foulspergame
+        self.xgperninety = xgperninety
+        self.shotspergame = shotspergame
+        self.xgpershot = xgpershot
+        self.keypasspergame = keypasspergame
+        self.dribblewonpergame = dribblewonpergame
+        self.rating = rating
+        self.interceptionpergame = interceptionpergame
+        self.goal = goal
+        self.assisttotal = assisttotal
--- deserealizer/player_deserializer.py
+++ deserealizer/player_deserializer.py
@@ -10,8 +10,21 @@ def deserialize_players(file_path : str):
         player_name = row["Player Name"]
         p = Player(
             name = player_name,
-            team = row["Player Team"]
+            team = row["Player Team"],position = row["Position 1"],
+            mins_played = row["Mins Played"],
+            tacklepergame = row["tacklePerGame"],
+            foulspergame = row["foulsPerGame"],
+            interceptionpergame = row["interceptionPerGame"],
+            xgperninety = row["xGPerNinety"],
+            shotspergame = row["shotsPerGame"],
+            xgpershot = row["xGPerShot"],
+            keypasspergame = row["keyPassPerGame"],
+            dribblewonpergame = row["dribbleWonPerGame"],
+            rating = row["rating"],
+            goal = row["goal"],
+            assisttotal = row["assistTotal"]
         )
         players[player_name] = p
+
     return players
 
